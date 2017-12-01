b0a8433fe3673a5f3f45182259ccabab07e81328
LidorioVinagreiro <33322949+LidorioVinagreiro@users.noreply.github.com>
2017-12-01 14:02:58 +0000

    01/12/2017 recuperarpassviewmodel

---
+++ ProjectoEsw/Models/ViewModels/RecuperarPassViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectoEsw.Models.ViewModels
+{
+    public class RecuperarPassViewModel
+    {
+        public string Email { get; set; }
+        public int Nif { get; set; }
+    }
+}

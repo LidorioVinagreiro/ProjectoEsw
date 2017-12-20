8473d32b28010baf16f734d360c4f96f67628030
LidorioVinagreiro <33322949+LidorioVinagreiro@users.noreply.github.com>
2017-12-20 16:07:41 +0000

    migration

--- ProjectoEsw/Migrations/20171220160103_initial.Designer.cs
+++ ProjectoEsw/Migrations/20171220160103_initial.Designer.cs
@@ -12,7 +12,7 @@ using System;
 namespace ProjectoEsw.Migrations
 {
     [DbContext(typeof(AplicacaoDbContexto))]
-    [Migration("20171219225811_initial")]
+    [Migration("20171220160103_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)

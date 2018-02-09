5fe0834ea3013517fb4d03ffca98c1d419b25b29
nando919 <32711099+nando919@users.noreply.github.com>
2018-02-09 13:09:54 +0000

    ultimo

--- TestesUnitarios/UnitTest1.cs
+++ TestesUnitarios/UnitTest1.cs
@@ -1,15 +1,33 @@
 using ProjectoEsw.Controllers;
 using System;
 using Xunit;
-
+using ProjectoEsw.GestorAplicacao;
+using ProjectoEsw.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using ProjectoEsw.Controllers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectoEsw.Models.Identity;
+using Microsoft.AspNetCore.Authorization;
+using ProjectoEsw.GestorAplicacao;
+using ProjectoEsw.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using ProjectoEsw.Models;
 namespace TestesUnitarios
 {
     public class UnitTest1
     {
+        private UserManager<Utilizador> user;
+        private SignInManager<Utilizador> ManegerS;
+        private AplicacaoDbContexto _context;
         [Fact]
         public void Test1()
         {
-            var controller = new HomeController();
+            GestorEmail gestorEmail = new GestorEmail();
+            Gestor gestor = new Gestor();
+            
+            
+            var controller = new HomeController(UserManager<Utilizador>, ManegerS, _context);
         }
     }
 }
